92637c9cc3e145ad7731c27796fe2d07f30f9686
thayashi1988 <tatsurouhayashi0210@gmail.com>
2021-01-11 23:40:28 +0900

    GitHub Pages deploy setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/nuxt-static-sample/favicon.ico' }],
   },
 
+  //
+  router: {
+    base: '/nuxt-static-sample/'
+  },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
