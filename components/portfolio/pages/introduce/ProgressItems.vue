4b9cf59939be76f5aecb11924186cc3b2fe013d9
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-01 00:08:52 +0900

    :construction:スキルのフェードイン実装中

--- components/portfolio/pages/introduce/ProgressItems.vue
+++ components/portfolio/pages/introduce/ProgressItems.vue
@@ -1,6 +1,11 @@
 <template>
   <li class="l-grid-col" data-col="3" data-col-sp="4" data-padding="true">
-    <div :id="progressData.skill" class="l-progress js-progress">
+    <div
+      :id="progressData.skill"
+      v-scroll="handleScroll"
+      :class="[{ 'is-show-bottom': scrollFlag }]"
+      class="l-progress js-progress"
+    >
       <p class="m-progress-txt">{{ progressData.skill }}</p>
       <p class="m-progress-ttl">
         <img :src="progressData.iconPath" :alt="progressData.skill" />
@@ -16,6 +21,18 @@ export default {
   props: {
     // eslint-disable-next-line
     progressData: Object,
+  },
+  data() {
+    return {
+      scrollFlag: false,
+    }
+  },
+  methods: {
+    handleScroll(evt, el) {
+      const top = el.getBoundingClientRect().top
+      const wh = window.innerHeight
+      if (top < wh / 1.5) this.scrollFlag = true
+    },
   },
 }
 </script>
