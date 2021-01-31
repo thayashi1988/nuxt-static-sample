4f867e06cd57a9a2f873fd313c371e86f7e277e9
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-01 00:06:56 +0900

    :construction:お問い合わせ、ブログのコンポーネント作成

---
+++ components/portfolio/pages/introduce/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-scroll="handleScroll"
+    :class="[{ 'is-show-bottom': scrollFlag }]"
+    class="js-contact is-hide"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      scrollFlag: false,
+    }
+  },
+  methods: {
+    handleScroll(evt, el) {
+      const top = el.getBoundingClientRect().top
+      const wh = window.innerHeight
+      if (top < wh / 1.5) this.scrollFlag = true
+    },
+  },
+}
+</script>
