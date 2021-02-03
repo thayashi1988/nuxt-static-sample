bf34ce4c1d16a20f8f59de9d337184f3ed9533b9
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-04 00:45:58 +0900

    :construction:お問い合わせページのcss修正、VeeValidateでのバリデートテスト

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -6,24 +6,33 @@
           <div class="l-contact">
             <the-contact>
               <template #formparts>
-                <form-input
-                  v-for="(formData, key) in formParts"
-                  :key="key"
-                  :id="formData.id"
-                  :type="formData.type"
-                  :autocomplete="formData.autocomplete"
-                  >{{ formData.label }}</form-input
+                <validation-provider
+                  v-slot="{ errors }"
+                  rules="required|max:100"
+                  name="お名前"
                 >
-                <form-text-area :id="formPartsTextArea.id">{{
-                  formPartsTextArea.label
-                }}</form-text-area>
+                  <form-input-name v-model.trim="nameModel"
+                    >お名前</form-input-name
+                  >
+                  <p v-show="errors.length" class="p-contact__error">
+                    {{ errors[0] }}
+                  </p>
+                </validation-provider>
+                <!-- <form-input-kana v-model.trim="nameKanaModel"
+                  >フリガナ</form-input-kana
+                > -->
+                <form-input-mail v-model.trim="mailModel"
+                  >メールアドレス</form-input-mail
+                >
+                <form-text-area v-model.trim="textareamodel"
+                  >お問い合わせ内容</form-text-area
+                >
               </template>
               <template #formspam>
-                <div v-show="false" class="p-contact__item">
+                <!-- <div v-show="false" class="p-contact__item">
                   <label for="message">スパムでない場合は空欄</label>
                   <input v-model="botField" type="text" name="bot-field" />
-                </div>
-                <!-- /.p-contact__item -->
+                </div> -->
               </template>
               <template #formbtn>
                 <form-submit-btn data-width="middle">送信</form-submit-btn>
@@ -37,149 +46,20 @@
     </the-main>
     <the-bread-crumb :bread-crumb-title="heading1"></the-bread-crumb>
   </div>
-
-  <!-- <div class="container">
-    <div>
-      <select id="selectA" ref="select1" name="select" @change="getValue(1)">
-        <option
-          v-for="(item, key) in test"
-          :key="key"
-          :value="item.value"
-          :selected="flag_a === item.value"
-        >
-          {{ item.value }}
-        </option>
-      </select>
-      <select id="selectB" ref="select2" name="select" @change="getValue(2)">
-        <option
-          v-for="(item, key) in test"
-          :key="key"
-          :value="item.value"
-          :selected="flag_b === item.value"
-        >
-          {{ item.value }}
-        </option>
-      </select>
-    </div>
-    <div class="flex mt-20">
-      <form class="w-full" name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  </div> -->
 </template>
 <script>
-import FormTextArea from '~/components/portfolio/ui/form/FormTextArea.vue'
-const aaa = []
-let ccc = []
-let duplicateFlag = false
-let pushNum = 0
-
 export default {
-  components: { FormTextArea },
   data() {
     return {
+      nameModel: '',
+      nameKanaModel: '',
+      mailModel: '',
+      textareamodel: '',
       heading1: 'お問い合わせ',
       heading1Sub: 'お仕事の依頼・技術的相談などお気軽にご連絡ください。',
-      formParts: [
-        {
-          type: 'text',
-          id: 'username',
-          autocomplete: 'name',
-          label: 'お名前',
-        },
-        {
-          type: 'text',
-          id: 'usernamekana',
-          autocomplete: 'namekana',
-          label: 'フリガナ',
-        },
-        {
-          type: 'text',
-          id: 'usermailadress',
-          autocomplete: 'usermailadress',
-          label: 'メールアドレス',
-        },
-      ],
-      formPartsTextArea: {
-        id: 'textarea',
-        label: 'お問い合わせ内容',
-      },
-      test: [
-        {
-          value: 'あああ',
-        },
-        {
-          value: 'いいい',
-        },
-        {
-          value: 'ううう',
-        },
-      ],
-      aaa: [
-        {
-          1: 'あああ',
-        },
-        {
-          2: 'いいい',
-        },
-        {
-          3: 'ううう',
-        },
-      ],
-      flag_a: 'いいい',
-      flag_b: 'ううう',
-      select: [],
     }
   },
   computed: {},
-  methods: {
-    getValue(index) {
-      // console.log(this.$refs["select" + index].getAttribute("id"));
-      // console.log(aaa.length);
-
-      const bbb = this.$refs['select' + index].getAttribute('id')
-      if (aaa.length > 0) {
-        for (let i = 0; i < aaa.length; i++) {
-          for (let num = 0; num < aaa.length; num++) {
-            ccc = []
-            ccc = Object.keys(aaa[num])
-            // console.log(bbb)
-            if (ccc[0] === bbb) {
-              duplicateFlag = true
-              pushNum = num
-              break
-            }
-          }
-          if (duplicateFlag) {
-            console.log('同じ値がある')
-            aaa[pushNum][bbb] = this.$refs['select' + index].value
-            duplicateFlag = false
-            break
-          } else {
-            console.log('同じ値がないっすすす')
-            aaa.push({ [bbb]: this.$refs['select' + index].value })
-            duplicateFlag = false
-          }
-        }
-      } else {
-        // 最初の選択のみ発火
-        aaa.push({ [bbb]: this.$refs['select' + index].value })
-      }
-
-      console.log(aaa)
-    },
-  },
+  methods: {},
 }
 </script>
