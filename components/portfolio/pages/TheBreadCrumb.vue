9e24d7ef7d4a845ce80c587b7ce292781a7d70ae
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-03 12:13:40 +0900

    :construction:パンくず修正

--- components/portfolio/pages/TheBreadCrumb.vue
+++ components/portfolio/pages/TheBreadCrumb.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="l-section l-section-last">
     <div class="l-section-inner">
-      <div id="l-breadcrumbs" class="l-breadcrumbs">
+      <div class="l-breadcrumbs">
         <ol
-          v-if="test.length > 0"
+          v-if="createBreadCrumb.length > 0"
           class="l-breadcrumbs-list"
           itemscope
           itemtype="https://schema.org/BreadcrumbList"
         >
           <li
-            class="l-breadcrumbs-item"
+            class="m-breadcrumbs-item"
             itemprop="itemListElement"
             itemscope
             itemtype="https://schema.org/ListItem"
@@ -20,32 +20,28 @@
             <meta itemprop="position" content="1" />
           </li>
           <li
-            v-for="(path, index) in test"
+            v-for="(path, index) in createBreadCrumb"
             :key="path"
-            class="l-breadcrumbs-item"
+            class="m-breadcrumbs-item"
             itemprop="itemListElement"
             itemscope
             itemtype="https://schema.org/ListItem"
           >
             <nuxt-link
-              v-if="index < test.length - 1"
+              v-if="index < createBreadCrumb.length - 1"
               itemprop="item"
               :to="`/` + path"
               ><span itemprop="name">{{ path }}</span></nuxt-link
             >
-            <span v-else itemprop="name">{{ path }}</span>
+            <span v-else itemprop="name">{{ breadCrumbTitle }}</span>
             <meta itemprop="position" :content="index + 2" />
           </li>
-          <!-- <li class="breadcrumbs__item">
-            <a href="https://pakumogu.sakura.ne.jp/top/blog/">
-              ぱくもぐブログ
-            </a>
-          </li> -->
         </ol>
       </div>
+      <!-- <p>{{ breadCrumbTitle }}</p> -->
       <!-- <p>{{ this.$route.path }}</p>
       <p>{{ this.$route.name }}</p>
-      <p>{{ test }}</p> -->
+      <p>{{ createBreadCrumb }}</p> -->
       <!-- #breadcrumbs.breadcrumbs -->
     </div>
   </div>
@@ -68,21 +64,13 @@ export default {
     },
   },
   computed: {
-    test() {
+    createBreadCrumb() {
       const path = this.$route.path
-      // const paths = []
-      // if (path !== '/') {
-      //   path = path.replace(/\/.*\//, '')
-      //   paths.push(path)
-      // }
-      const paths = path.split('/')
-      // console.log(paths)
-      // paths.push({ to: path.split('/'), name: path.split('/') })
-      // const slashLen = path.split('/')
-      // while (path !== '/') {
-      //   paths.push(path)
-      //   path = path.replace(/[^\/]+\/$/, '')
-      // }
+      const pathsArray = path.split('/')
+      // console.log(pathsArray)
+      const pathsOrthopaedy = pathsArray.filter((str) => str !== '')
+      const paths = pathsOrthopaedy.filter((str) => str !== 'article')
+      console.log(paths)
       return paths
     },
   },
