aed1ecabf1b07a6dc4eaaa30aa8ea6abf5ba4490
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-04 18:57:45 +0900

    :construction:隠しフォームの設置場所を修正、axiosをコメントアウト、proxy消してみる

--- components/portfolio/layouts/TheContact.vue
+++ components/portfolio/layouts/TheContact.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="l-contact">
-    <slot name="formparts">
-      <input type="hidden" name="form-name" value="contact" />
-    </slot>
+    <slot name="formparts"></slot>
     <slot name="formspam"></slot>
     <slot name="formbtn"></slot>
   </div>
