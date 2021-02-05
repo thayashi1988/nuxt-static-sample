f9792d69a4965b99902210a590905ab2dc2bd616
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-06 00:39:37 +0900

    :wrench:Cardの子コンポーネントを追加

---
+++ components/portfolio/ui/card/CardBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="l-btn">
+    <nuxt-link :to="to" class="m-btn m-card-btn" :data-width="dataWidth">
+      <slot></slot>
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardBtn',
+  props: {
+    to: {
+      type: String,
+      default: '',
+    },
+    dataWidth: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
---
+++ components/portfolio/ui/card/CardDate.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="m-card-type">
+    <span class="m-card-date"
+      >初稿：<slot name="date">{{ date }}</slot></span
+    >
+    <span v-if="upDateBoolean" class="m-card-date m-card-date-update"
+      >更新：<slot name="upDate">{{ upDate }}</slot></span
+    >
+    <!-- <span class="m-label"><a href="/single/">ラベル</a></span> -->
+  </p>
+</template>
+<script>
+export default {
+  name: 'CardDate',
+  props: {
+    date: {
+      type: String,
+      default: '',
+    },
+    upDate: {
+      type: String,
+      default: '',
+    },
+    upDateBoolean: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
