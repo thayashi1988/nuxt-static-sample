8174cf80b5d9ecaafc67063ac74fbcfcaa6b84cd
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-24 18:45:18 +0900

    :sparkles:記事詳細ページに次の記事、前の記事を表示できるように追加

---
+++ components/portfolio/layouts/TheArticles.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="l-article-links">
+    <the-articles-prev
+      v-show="prevShowFlag"
+      :prev-flag="prevShowFlag"
+      :prev-href="prevObject.id"
+      :prev-src="prevObject.thumbImg.url"
+      :prev-title="prevObject.title"
+    ></the-articles-prev>
+    <the-articles-next
+      v-show="nextShowFlag"
+      :next-flag="nextShowFlag"
+      :next-href="nextObject.id"
+      :next-src="nextObject.thumbImg.url"
+      :next-title="nextObject.title"
+    ></the-articles-next>
+  </ul>
+  <!-- ./l-artcle-links -->
+</template>
+
+<script>
+export default {
+  name: 'TheArticles',
+  props: {
+    nextObject: {
+      type: Object,
+      default: () => {},
+    },
+    nextShowFlag: {
+      type: Boolean,
+      default: false,
+    },
+    prevObject: {
+      type: Object,
+      default: () => {},
+    },
+    prevShowFlag: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
