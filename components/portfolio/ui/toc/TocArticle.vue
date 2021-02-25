20247b80df0b766b2f970498d2bb30cf69fdb190
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-02-25 15:26:20 +0900

    :sparkles:記事詳細に目次を表示

---
+++ components/portfolio/ui/toc/TocArticle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="l-toc">
+    <h3 class="m-heading-3 m-mb-0">目次</h3>
+    <ul class="l-toc-list">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TocArticle',
+}
+</script>
