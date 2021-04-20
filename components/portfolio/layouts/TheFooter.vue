9bfbd2489bf9e24c1cb3f561d13742154a001f91
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-04-20 12:28:01 +0900

    ✨ コピーライトの年を関数化

--- components/portfolio/layouts/TheFooter.vue
+++ components/portfolio/layouts/TheFooter.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="l-footer">
     <div class="l-footer-inner">
-      <p class="m-copy">Copyright &copy; 2021 pakumoguBlog</p>
+      <p class="m-copy">
+        Copyright &copy; 2021 ~ {{ getCurrentYear }}pakumoguBlog
+      </p>
     </div>
     <!-- ./l-footer-inner -->
   </footer>
@@ -10,5 +12,11 @@
 <script>
 export default {
   name: 'TheFooter',
+  computed: {
+    getCurrentYear() {
+      const currentYear = new Date().getFullYear()
+      return currentYear
+    },
+  },
 }
 </script>
