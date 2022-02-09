e73a830f1e45d5ef6aecfb796bed0e5f7c79bb45
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-09 15:13:04 +0900

    ♻️ パンくずようにも処理追加

--- plugins/microCMS/headingBreak.js
+++ plugins/microCMS/headingBreak.js
@@ -8,12 +8,13 @@ const args = {
 // タイトルの改行対応
 Vue.prototype.$myInjectedFunction = (heading, flag) => {
   let headingTxt = heading
-  if (flag) {
+  if (flag === 'heading1') {
     headingTxt = headingTxt.replace(/<br>/g, '<br>')
-    return headingTxt
+  } else if (flag === 'bread') {
+    headingTxt = headingTxt.replace(/<br>/g, '')
   } else {
     headingTxt = headingTxt.replace(/<br>/g, '')
     headingTxt = headingTxt.replace(/】/, '】<br>')
-    return headingTxt
   }
+  return headingTxt
 }
