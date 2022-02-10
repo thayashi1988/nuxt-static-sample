6757ef058e4fc49686b2406120bd9db777a4fcc7
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-10 21:23:02 +0900

    🐛 Heading2にv-htmlするとエラーになるので、記事詳細の箇所だけv-ifで出し分ける

--- components/portfolio/ui/heading/Heading2.vue
+++ components/portfolio/ui/heading/Heading2.vue
@@ -1,16 +1,22 @@
 <template>
-  <h2 class="m-heading-2" :data-type="dataType">
+  <h2 v-if="!br" class="m-heading-2" :data-type="dataType">
     {{ headingSecondTitle }}
     <slot />
     <span v-if="dataType === ''" class="m-header-txtsub">
       {{ headingSecondSubTitle }}
     </span>
   </h2>
+  <h2 v-else class="m-heading-2" :data-type="dataType"></h2>
 </template>
 <script>
 export default {
   name: 'Heading2',
   props: {
+    br: {
+      type: Boolean,
+      default: false,
+      require: false,
+    },
     dataType: {
       type: String,
       default: '',
